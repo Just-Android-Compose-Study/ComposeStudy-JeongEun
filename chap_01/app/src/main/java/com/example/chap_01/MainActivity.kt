16ddd33dce91e88414b5530c781c17707ca34087
THINKBIG\10017014 <jjeee1313@gmail.com>
2023-02-09 17:33:29 +0900

    [FEAT] Hello 실습

--- chap_01/app/src/main/java/com/example/chap_01/MainActivity.kt
+++ chap_01/app/src/main/java/com/example/chap_01/MainActivity.kt
@@ -3,41 +3,143 @@ package com.example.chap_01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.chap_01.ui.theme.Chap_01Theme
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Chap_01Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Hello()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(group = "my-group-1", device = Devices.AUTOMOTIVE_1024p)
+fun GreetingWrapper() { // 매개변수 있는 컴포저블 함수를 감싸서 미리보기 -> 구림
+    Greeting("Jetpack Compose")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Chap_01Theme {
-        Greeting("Android")
+@Preview(group = "my-group-1")
+fun AltGreeting(name: String = "Jetpack Compose") { // 컴포저블 함수에 기본값 추가 - 기본값을 정의하지 않은 이유가 있는 경우 부적절
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
+
+class HelloProvider : PreviewParameterProvider<String> {
+    override val values: Sequence<String>
+        get() = listOf("PreviewParameterProvider").asSequence()
+}
+
+@Composable
+@Preview
+fun AltGreeting2(@PreviewParameter(HelloProvider::class)
+                 name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
+
+@Composable
+@Preview
+fun Welcome(){
+    Text(text = stringResource(id = R.string.welcome))
+}
+
+@Composable
+fun Greeting(name: String){
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
+
+@Composable
+fun TextAndButton(name: MutableState<String>, nameEntered: MutableState<Boolean>) {
+    Row(modifier = Modifier.padding(top = 8.dp)) {
+        TextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.hint))
+            },
+            modifier = Modifier
+                .alignByBaseline()
+                .weight(1.0F),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words,
+            ),
+            keyboardActions = KeyboardActions(onAny = {
+                nameEntered.value = true
+            })
+        )
+        Button(modifier = Modifier
+            .alignByBaseline()
+            .padding(8.dp),
+            onClick = {
+                nameEntered.value = true
+            }) {
+            Text(text = stringResource(id = R.string.done))
+        }
     }
 }
+
+@Composable
+fun Hello() {
+    val name = remember { mutableStateOf("") }
+    val nameEntered = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (nameEntered.value) {
+            Greeting(name.value)
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Welcome()
+                TextAndButton(name, nameEntered)
+            }
+        }
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    StudyChap1Theme {
+//        Greeting("Android")
+//    }
+//}
--- chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
+++ chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
@@ -1,43 +1,64 @@
-package com.example.chap_02
+package eu.thomaskuenneth.composebook.hello
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.chap_02.ui.theme.Chap_02Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Chap_02Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Factorial()
+        }
+    }
+}
+
+@Composable
+fun Factorial() {
+    var expanded by remember { mutableStateOf(false) }
+    var text by remember { mutableStateOf(factorialAsString(0)) }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.clickable {
+                expanded = true
+            },
+            text = text,
+            style = MaterialTheme.typography.headlineMedium
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }) {
+            for (n in 0 until 10) {
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    text = factorialAsString(n)
+                }) {
+                    Text("${n.toString()}!")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Chap_02Theme {
-        Greeting("Android")
+fun factorialAsString(n: Int): String {
+    var result = 1L
+    for (i in 1..n) {
+        result *= i
     }
+    return "$n! = $result"
 }
