52f16803f8483bbcb41ddf30c8574f1440da0cb2
THINKBIG\10017014 <jjeee1313@gmail.com>
2023-02-13 16:27:39 +0900

    [CHORE] 프로젝트 생성

--- chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
+++ chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
@@ -3,64 +3,41 @@ package com.example.chap_02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.chap_02.ui.theme.Chap_02Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Factorial()
-        }
-    }
-}
-
-@Composable
-@Preview
-fun Factorial() {
-    var expanded by remember { mutableStateOf(false) }
-    var text by remember { mutableStateOf(factorialAsString(0)) }
-    Box(
-        modifier = Modifier.fillMaxSize(), // 박스 사이즈
-        contentAlignment = Alignment.Center // content 정렬
-    ) {
-        Text(
-            modifier = Modifier.clickable {
-                expanded = true
-            },
-            text = text,
-            style = MaterialTheme.typography.h2
-        )
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { // 아무것도 선택하지 않고 닫음
-                expanded = false
-            }) {
-            for (n in 0 until 10){
-                DropdownMenuItem(onClick = {
-                    expanded = false
-                    text = factorialAsString(n)
-                }) {
-                    Text("${n.toString()}!")
+            Chap_02Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-fun factorialAsString(n: Int): String {
-    var result = 1L
-    for (i in 1..n) {
-        result *= i
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Chap_02Theme {
+        Greeting("Android")
     }
-    return "$n! = $result"
 }
