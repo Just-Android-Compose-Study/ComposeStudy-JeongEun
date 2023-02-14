0dad40cf2c7e45d5fd00f8800231e66f91304b1e
THINKBIG\10017014 <jjeee1313@gmail.com>
2023-02-14 14:44:36 +0900

    [FEAT] Factorial 실습

--- chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
+++ chap_02/app/src/main/java/com/example/chap_02/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.chap_02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.chap_02.ui.theme.Chap_02Theme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +26,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 @Preview
-fun Factorial(){
-    var expanded by remem
+fun Factorial() {
+    var expanded by remember { mutableStateOf(false) }
+    var text by remember { mutableStateOf(factorialAsString(0)) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        contentAlignment = Alignment.Center,
+        content = {
+            Column {
+                Text(
+                    modifier = Modifier.clickable {
+                        expanded = true
+                    },
+                    text = text,
+                    style = MaterialTheme.typography.h2
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {
+                        expanded = false
+                    }) {
+                    for (n in 0 until 10){
+                        DropdownMenuItem(onClick = {
+                            expanded = false
+                            text = factorialAsString(n)
+                        }) {
+                            Text("${n.toString()}")
+                        }
+                    }
+                }
+            }
+        }
+    )
 }
 
-fun factorialAsString(n: Int): String{
+fun factorialAsString(n: Int): String {
     var result = 1L
-    for (i in 1..n){
+    for (i in 1..n) {
         result *= i
     }
     return "$n! = $result"
