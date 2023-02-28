bd4195989454a36d7597fb44fa9439f634f06583
THINKBIG\10017014 <jjeee1313@gmail.com>
2023-02-28 13:59:52 +0900

    [FEAT] 상태 유무 컴포저블

--- chap_05/app/src/main/java/com/example/chap_05/MainActivity.kt
+++ chap_05/app/src/main/java/com/example/chap_05/MainActivity.kt
@@ -3,41 +3,86 @@ package com.example.chap_05
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chap_05.ui.theme.Chap_05Theme
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Chap_05Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+//            SimpleStateDemo1()
+//            RememberObserver()
+//            SimpleStatelessComposable1()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview
+fun SimpleStateDemo1(){
+    // num은 값 홀더의 참조 가짐.
+    // num.value로 숫자를 얻고, 값 변경 가능
+    val num =  remember { mutableStateOf(Random.nextInt(0, 10)) }
+    Text(text = num.value.toString())
+
+    // by 사용 -> 값을 저장(상태 자신을 num2에 할당x)
+    // num2를 변경하려면 var
+    val num2 by remember { mutableStateOf(Random.nextInt(0, 10)) }
+    Text(text = num2.toString())
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Chap_05Theme {
-        Greeting("Android")
+@Preview
+fun RememberObserver(){
+    var key by remember { mutableStateOf(false) }
+    val date by remember(key) { mutableStateOf(Date()) }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(date.toString())
+        Button(onClick = { key != key }) {
+            Text(text = "click")
+        }
     }
 }
+
+@Composable
+fun SimpleStatelessComposable1(){
+    Text(text = "Hello Compose")
+}
+
+@Composable
+fun SimpleStatelessComposable2(text: State<String>){
+    Text(text = text.value)
+}
+
+// stateless
+@Composable
+fun TextFieldDemo(state: MutableState<TextFieldValue>) {
+    TextField(
+        value = state.value,
+        onValueChange = { state.value = it },
+        placeholder = { Text("Hello") },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+// stateful
+@Composable
+@Preview
+fun TextFieldDemo() {
+    val state = remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    TextFieldDemo(state)
+}
+
