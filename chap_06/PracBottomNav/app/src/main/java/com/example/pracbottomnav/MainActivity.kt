44f7959b736bbf5d83699c75cbd6e8fa594e072b
THINKBIG\10017014 <jjeee1313@gmail.com>
2023-03-07 17:01:48 +0900

    [CHORE] 프로젝트 생성

---
+++ chap_06/PracBottomNav/app/src/main/java/com/example/pracbottomnav/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.pracbottomnav
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pracbottomnav.ui.theme.PracBottomNavTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PracBottomNavTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PracBottomNavTheme {
+        Greeting("Android")
+    }
+}
